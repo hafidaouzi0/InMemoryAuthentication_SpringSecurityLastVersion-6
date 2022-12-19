a9413fec80617d373530bf657ca0118d7ce0d971
hafidaouzi0 <faouzibrahim2014@gmail.com>
2022-12-19 20:49:24 +0100

    3d commit

--- newVersionspringSecurity6/src/main/java/sec/tuto/NewVersionspringSecurity6Application.java
+++ newVersionspringSecurity6/src/main/java/sec/tuto/NewVersionspringSecurity6Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewVersionspringSecurity6Application implements CommandLineRunner {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(NewVersionspringSecurity6Application.class, args);
 	}
